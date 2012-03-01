12226e85f2b7071f6def2d375f1892bdb09b9741
Pratik <pratik@Buzzinga.(none)>
2012-03-01 16:13:27 +0200

    Admin

---
+++ plan_proposals/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import PlanningProject,Image,Proposal
+
+admin.site.register(PlanningProject)
+admin.site.register(Image)
+admin.site.register(Proposal) 
