505322e778e1ddb8bef64f0b46c2021427f2ff33
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-07-17 09:59:30 +0300

    translations updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='plan_proposals',
-    version='4.1.0',
+    version='4.1.1',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/plan_proposal',
     packages=find_packages(),
