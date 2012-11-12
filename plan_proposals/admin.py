6047245ca5996d93640fa6f885ec5ca6dbab1712
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-11-12 11:29:16 +0200

    the admin now uses local openlayers

--- plan_proposals/admin.py
+++ plan_proposals/admin.py
@@ -1,5 +1,8 @@
 from django.contrib.gis import admin
-from models import PlanningProject,Image,Proposal
+from django.core.urlresolvers import reverse_lazy
+from plan_proposals.models import PlanningProject
+from plan_proposals.models import Image
+from plan_proposals.models import Proposal
 from django.conf import settings
 
 class ProjectAdmin(admin.OSMGeoAdmin):
@@ -12,7 +15,11 @@ class ProjectAdmin(admin.OSMGeoAdmin):
     default_zoom = getattr(settings,
                           'ORGANIZATION_ADMIN_DEFAULT_MAP_SETTINGS',
                           {'default_zoom': 4})['default_zoom']
+    
+    openlayers_url = '%s%s' % (getattr(settings, 'STATIC_URL', '/'),
+                               'js/libs/OpenLayers.js')
+    extra_js = (reverse_lazy('osmextra'),)
                           
-admin.site.register(PlanningProject,ProjectAdmin)
+admin.site.register(PlanningProject, ProjectAdmin)
 admin.site.register(Image)
 admin.site.register(Proposal)
