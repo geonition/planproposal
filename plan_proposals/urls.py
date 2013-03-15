81399c6ebbe05d8b9eac8c920a5caa290e0314da
Mikko Johansson <mikko.johansson@aalto.fi>
2013-03-15 13:12:33 +0200

    Added start and end date and map to planning project. Active planning projects can be retrieved through rest interface.

--- plan_proposals/urls.py
+++ plan_proposals/urls.py
@@ -3,12 +3,15 @@ from django.conf.urls.defaults import url
 from django.conf import settings
 
 urlpatterns = patterns('plan_proposals.views',
+    url(r'^active/$',
+        'get_active_planning_projects',
+        name='active_planning_projects'),
 
-    url(r'^project/(?P<project_name>[\w+(+-_)*]+)/$',
+    url(r'^project/(?P<project_slug>[\w+(+-_)*]+)/$',
         'planning_project',
         name='planning_project'),
 
-    url(r'^proposal/(?P<project_name>[\w+(+-_)*]+)/(?P<proposal_name>[\w+(+-_)*]+)/$',
+    url(r'^proposal/(?P<project_slug>[\w+(+-_)*]+)/(?P<proposal_slug>[\w+(+-_)*]+)/$',
         'plan_proposal',
         name='plan_proposal'),    
     )
